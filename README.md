addbe448fdb8e2331ffcde3fba2cc22beab3e53b
Harsh Jain <harshjniitr@gmail.com>
2020-09-08 19:11:18 +0300

    Support witnet 0.9.3

--- README.md
+++ README.md
@@ -18,8 +18,8 @@ from witnet_lib.utils import AttrDict
 
 # Setting config
 config = AttrDict({
-    "genesis_sec": 1597827600,
-    "magic": 50178,
+    "genesis_sec": 1599555600,
+    "magic": 3029,
     "sender_addr": "127.0.0.1:21341",
     "time_per_epoch": 45,
 })
@@ -65,8 +65,8 @@ Mapping all nodes in the network (DAG fashion)
 from witnet_lib import utils
 from witnet_lib.map_nodes  import MapNodes
 config = utils.AttrDict({
-    "genesis_sec": 1597827600,
-    "magic": 50178,
+    "genesis_sec": 1599555600,
+    "magic": 3029,
     "sender_addr": "127.0.0.1:21341",
     "time_per_epoch": 45,
 })
--- main.py
+++ main.py
@@ -3,12 +3,15 @@ from witnet_lib import utils
 
 if __name__ == "__main__":
     config = utils.AttrDict({
-        "genesis_sec": 1597827600,
-        "magic": 50178,
+        "genesis_sec": 159555600,
+        "magic": 3029,
         "sender_addr": "127.0.0.1:21341",
         "time_per_epoch": 45,
     })
     mapper = MapNodes(config, ["127.0.0.1:21337"])
     all_nodes, active_nodes = mapper.start_mapping_workers(3)
     print(all_nodes)
-    print("Acitve nodes:", active_nodes)
+    with open("active.json",'w') as f:
+        json.dump(active_nodes, f)
+    with open('all_nodes.json', 'w') as f:
+        json.dump(list(all_nodes), f)
